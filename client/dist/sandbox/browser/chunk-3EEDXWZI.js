d4bcfa60cd324ac7544b6d3689600ef2e34837f2
Woolwich1886 <zakharov-max4@mail.ru>
2024-05-25 22:09:52 +0700

    fix gitignore

--- client/dist/sandbox/browser/chunk-3EEDXWZI.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./chunk-HWALBNDX.js";import"./chunk-BDBREKTO.js";import"./chunk-D75ILFX5.js";import"./chunk-BF5C4A5R.js";export{a as MessengerModule};
--- client/dist/sandbox/browser/chunk-H55P7ACM.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-7SO5VYT4.js";import"./chunk-BDBREKTO.js";import"./chunk-BF5C4A5R.js";export{a as SpheresModule};
